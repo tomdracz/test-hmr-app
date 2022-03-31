3a09e04f94a3b5360a04f9bf513367c797e25fae
Tom Dracz <tom.dracz@sanctus.io>
2022-03-31 10:35:32 +0100

    Add webpack config and proper version of refresh plugin

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,15 @@
-const { webpackConfig } = require('shakapacker')
+const { webpackConfig, inliningCss, merge } = require('shakapacker');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
-// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+if (isDevelopment && inliningCss) {
+  webpackConfig.plugins.push(
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockPort: webpackConfig.devServer.port,
+      },
+    })
+  );
+}
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
