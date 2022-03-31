0f2c8f429c171a3bd43ac55e18416718b064a35a
Tom Dracz <tom.dracz@sanctus.io>
2022-03-31 10:36:39 +0100

    Move to separate component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './App.css';
+import Test from './test';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 document.addEventListener('DOMContentLoaded', () => {
-  root.render(<p>Hello, World!!</p>);
+  root.render(<Test />);
 });
---
+++ app/javascript/test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <p>Hello worlds!!!!!</p>
+  )
+}
+
+export default Test;
